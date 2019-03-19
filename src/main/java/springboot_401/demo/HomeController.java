85e09024a8c21351d3f2ec11e21d057ac1ac46cc
jukegbu1 <jukegbu1@gmail.com>
2019-03-19 09:31:36 -0400

    Initial commit

---
+++ src/main/java/springboot_401/demo/HomeController.java
@@ -0,0 +1,22 @@
+package springboot_401.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+
+
+
+
+
+
+
+}
